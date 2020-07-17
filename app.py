7fcc2c68d47bfe572692e3583d88292c62a496cd
EricWeathers-ux <eric.weathers@bklynlab.org>
2020-07-17 14:01:46 +0000

    fixes

--- app.py
+++ app.py
@@ -19,11 +19,19 @@ from random import randint
 @app.route('/riddle')
 def riddle():
     global riddle2
+    global random_number
+    global riddles
+    global codes
+    global Riddle
     riddles=[{"riddle": ["What starts with I and ends with phone?", "Iphone"]},
-    {"riddle": ["What starts with t ends with t and has t in it?", "teapot"]}
+    {"riddle": ["What starts with t ends with t and has t in it?", "teapot"]},
+    {"riddle": ["What color is the sky?", "blue"]}
     ]
-    Riddle=riddles[randint(-1,1)]
+    random_number=randint(-1,1)
+    Riddle=riddles[random_number]
     riddle2=Riddle
+    print("RIDDLE2:")
+    print(riddle2)
     codes=[no_vowels(Riddle)]
     return render_template("riddle.html", codes=codes[0])
 def no_vowels(string):
@@ -36,12 +44,27 @@ def no_vowels(string):
     # print(string2)
     return string2
 riddle2=""
+random_number=randint(-1,1)
+riddles=[{"riddle": ["What starts with I and ends with phone?", "Iphone"]},
+    {"riddle": ["What starts with t ends with t and has t in it?", "teapot"]},
+    {"riddle": ["What color is the sky?", "blue"]}
+]
+Riddle=riddles[random_number]
+codes=[no_vowels(Riddle)]
 @app.route('/code')
 def code():
     return render_template("code.html")
 
 @app.route('/solve_riddle', methods=['GET', 'POST'])
 def solve_riddle():
-    print("RIDDLE2:")
-    print(riddle2)
-    return render_template("solve_riddle.html", riddle2=riddle2)
+    if request.form['riddle_input'] == Riddle["riddle"][1]:
+        random_number=randint(-1,2)
+        riddles=[{"riddle": ["What starts with I and ends with phone?", "Iphone"]},
+        {"riddle": ["What starts with t ends with t and has t in it?", "teapot"]},
+        {"riddle": ["What color is the sky?", "blue"]}
+        ]
+        print("riddles[random_number]:")
+        print(riddles[random_number])
+        return render_template("solve_riddle.html", riddle2=riddles[random_number])
+    else:
+        return render_template("riddle.html", codes=codes[0])
